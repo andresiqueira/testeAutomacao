3e24150a8f97fb82b5c7fb2fde4afbc3c1013850
Leonardoabdalla <leonardoabdalladeveloper@gmail.com>
2022-10-17 18:54:56 -0300

    corrigindo a requisição

--- src/apiTrello/api.ts
+++ src/apiTrello/api.ts
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
 
 const trelloApi = async () => {
-    const response = await fetch('https://api.trello.com/1/cards/?key=5032955848a8a601c3140529435ee9c9&token=55c29e64f866451d79a93781877b5c886f398bfd50915c38dbd314aa4c64c2ac');
+    const response = await fetch('https://api.trello.com/1/cards/634da0935372cf02b516b97a?key=5032955848a8a601c3140529435ee9c9&token=55c29e64f866451d79a93781877b5c886f398bfd50915c38dbd314aa4c64c2ac');
     const dados = await response.json();
-    console.log(dados);
+    return dados;
+    // console.log(dados);
 }
 
 export default trelloApi;
