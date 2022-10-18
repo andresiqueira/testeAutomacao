ba438f0206851c9fbb98f3598371ad3d9f7ab182
Leonardoabdalla <leonardoabdalladeveloper@gmail.com>
2022-10-18 16:37:18 -0300

    adicionando fetch de lists

--- src/apiTrello/api.ts
+++ src/apiTrello/api.ts
@@ -2,12 +2,12 @@ import fetch from 'node-fetch';
 require('dotenv').config();
 
 
-const ID_CARD = process.env.ID_CARD;
+const ID_LIST = process.env.ID_LIST;
 const KEY = process.env.KEY;
 const TOKEN = process.env.TOKEN;
 
 const trelloApi = async () => {
-    const response = await fetch(`https://api.trello.com/1/cards/${ID_CARD}?key=${KEY}&token=${TOKEN}`);
+    const response = await fetch(`https://api.trello.com/1/lists/${ID_LIST}?key=${KEY}&token=${TOKEN}`);
     const dados = await response.json();
     return dados;
 }
